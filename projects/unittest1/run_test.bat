5d98b749d115357463873b17c981570bec720dcc
gengfo <gengfo@gmail.com>
2020-03-21 16:26:10 +0800

    python unit test

---
+++ projects/unittest1/run_test.bat
@@ -0,0 +1,9 @@
+rem #python -m unittest test_module1 test_module2 
+
+rem #同时测试多个module
+rem #python -m unittest test_module.TestClass
+rem #python -m unittest test_module.TestClass.test_method
+
+rem #python -m unittest -v test_module
+
+python -m unittest tests/unittest1_tests.py
--- projects/unittest1/tests/unittest1_tests.py
+++ projects/unittest1/tests/unittest1_tests.py
@@ -3,5 +3,8 @@ import unittest
 
 
 class MainTest(unittest.TestCase):
-    def test_main(self):
-        self.assertEqual(3, main.add(1, 2))
+    def test_add1(self):
+    	self.assertEqual(3, main.add(1, 2))
+
+    def test_add2(self):
+    	self.assertNotEqual(4, main.add(1, 2))
