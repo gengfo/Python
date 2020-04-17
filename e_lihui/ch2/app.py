0d68aed6e9770c585f7e7c5f3ebcea06f44cf14c
gengfo <gengfo@gmail.com>
2020-04-17 09:06:29 +0800

    [flask] lihui

--- e_lihui/ch2/app.py
+++ e_lihui/ch2/app.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, redirect, url_for, make_response
 
 
 app = Flask(__name__)
+
+@app.route('/say')
+def hi():
+    return  redirect(url_for('say_hello'))
 
 @app.route('/')
 def index():
@@ -11,7 +15,7 @@ def index():
 
 # bind multiple URL for one view function
 @app.route('/hi')
-@app.route('/hello')
+#@app.route('/hello')
 def say_hello():
     return '<h1>Hello, Flask!</h1>'
 
@@ -19,7 +23,13 @@ def say_hello():
 @app.route('/greet', defaults={'name': 'Programmer'})
 @app.route('/greet/<name>')
 def greet(name):
-    return '<h1>Hello, %s!</h1>' % name    
+    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/set/<name>')
+def set_cookie(name):
+    response = make_response(redirect(url_for('say_hello')))
+    response.set_cookie('name',name)
+    return  response
 
 if __name__ == '__main__' :
 	app.run(port=5000, debug=True)
