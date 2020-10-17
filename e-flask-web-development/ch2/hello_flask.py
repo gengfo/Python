9802d29de2ecde3a8de802dd5b58dc3c0233f9bd
GengFo <gengfo@oocl.com>
2020-10-17 10:57:36 +0800

    add hello world

---
+++ e-flask-web-development/ch2/hello_flask.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/hello')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
